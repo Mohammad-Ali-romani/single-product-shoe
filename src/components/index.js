160b72b9222188176fda02a6a3185576fe9af005
mohammad ali <mohammad.ali.romani@gmail.com>
2022-11-15 08:22:33 +0200

    add files build

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import BodyProduct from "./BodyProduct/BodyProduct";
-import SliderImage from "./SliderImage/SliderImage";
-import Info from "./Info/Info";
-import Colors from "./Colors/Colors";
-
-export { BodyProduct, SliderImage, Info ,Colors}
--- src/layouts/index.js
+++
@@ -1,3 +0,0 @@
-import Content from "./Content/Content";
-import Navbar from "./Navbar/Navbar";
-export {Content, Navbar};
